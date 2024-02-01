9d1cb28fb3840fbda8b83ed2347af20867c4dda8
nfkhanGit <88129520+nfkhanGit@users.noreply.github.com>
2024-02-01 14:27:55 +0400

    Add files via upload

---
+++ AppTester/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace AppTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFromEmail = new System.Windows.Forms.TextBox();
+            this.txtToEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFromPW = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCCEmail = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From Email";
+            // 
+            // txtFromEmail
+            // 
+            this.txtFromEmail.Location = new System.Drawing.Point(131, 30);
+            this.txtFromEmail.Name = "txtFromEmail";
+            this.txtFromEmail.Size = new System.Drawing.Size(572, 31);
+            this.txtFromEmail.TabIndex = 1;
+            // 
+            // txtToEmail
+            // 
+            this.txtToEmail.Location = new System.Drawing.Point(131, 74);
+            this.txtToEmail.Name = "txtToEmail";
+            this.txtToEmail.Size = new System.Drawing.Size(572, 31);
+            this.txtToEmail.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "To Email";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(131, 170);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(572, 31);
+            this.txtSubject.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Subject";
+            // 
+            // txtFromPW
+            // 
+            this.txtFromPW.Location = new System.Drawing.Point(131, 126);
+            this.txtFromPW.Name = "txtFromPW";
+            this.txtFromPW.Size = new System.Drawing.Size(572, 31);
+            this.txtFromPW.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "From PW";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(292, 419);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(112, 34);
+            this.btnSend.TabIndex = 8;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(131, 224);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(572, 31);
+            this.txtHost.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Host";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(131, 261);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(572, 31);
+            this.txtPort.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Port";
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(131, 350);
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(572, 31);
+            this.txtBody.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 350);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 25);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Body";
+            // 
+            // txtCCEmail
+            // 
+            this.txtCCEmail.Location = new System.Drawing.Point(131, 313);
+            this.txtCCEmail.Name = "txtCCEmail";
+            this.txtCCEmail.Size = new System.Drawing.Size(572, 31);
+            this.txtCCEmail.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 316);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 25);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "CC Email";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 490);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCCEmail);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtFromPW);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtToEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFromEmail);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Email Tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFromEmail;
+        private System.Windows.Forms.TextBox txtToEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFromPW;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCCEmail;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ AppTester/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            bool isSent = false;
+            try
+            {
+                MailMessage message = new MailMessage();
+                MailAddressCollection ma = new MailAddressCollection();
+                //message.To.Add(BussinessAccessLayer.LoginEmailID);
+                //message.Bcc.Add(BussinessAccessLayer.LoginEmailID);
+
+
+
+                foreach (var item in txtToEmail.Text.Trim().TrimEnd(',').Split(','))
+                {
+                    message.To.Add(item);
+                }
+                if (!string.IsNullOrEmpty(txtCCEmail.Text))
+                {
+                    foreach (var item in txtCCEmail.Text.Trim().TrimEnd(',').Split(','))
+                    {
+                        message.CC.Add(item);
+                    }
+                }
+
+
+
+                if (string.IsNullOrEmpty(txtFromEmail.Text))
+                {
+                    ma.Add(new MailAddress("test@gmail.com", "Name: Test"));
+                    message.From = ma[0];
+                    message.Sender = ma[0];
+                    message.ReplyToList.Add(ma[0]);
+                }
+                else
+                {
+                    ma.Add(txtFromEmail.Text);
+                    message.From = new MailAddress(txtFromEmail.Text);
+                    message.Sender = new MailAddress(txtFromEmail.Text);
+                    message.ReplyToList.Add(txtFromEmail.Text);
+                }
+
+                message.Subject = txtSubject.Text;
+                message.Body = txtBody.Text;
+                message.IsBodyHtml = true;
+
+
+                SmtpClient smtp = new SmtpClient(txtHost.Text, Convert.ToInt32(txtPort.Text));
+                try
+                {
+                    smtp.Send(message);
+                    isSent = true;
+                    MessageBox.Show("Success");
+                }
+                catch (SmtpException se)
+                {
+                    MessageBox.Show("SMTP Exc" + se.ToString());
+                    //BussinessLayer.LogAppErrors("MailSender", "SendSMTPEMail", "", se.ToString(), "");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed" + ex.ToString());
+            }
+        }
+    }
+}
